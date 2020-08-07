9850517398ff51b7542caecf9e6b8f8878ee6615
dthecsguy <50031416+dthecsguy@users.noreply.github.com>
2020-08-06 20:31:44 -0700

    Update tests.py

--- test/tests.py
+++ test/tests.py
@@ -16,10 +16,15 @@
 # altered in between executions (unless preconditions are used).
 tests = [{'description': 'Testing functionality', 'steps': [ 
         {'inputs': [('PINA', 0x01)], 'iterations': 5},
-        {'inputs': [('PINA', 0x00)], 'iterations': 5, 'expected': [('PORTB', 0x02)]},
+        {'inputs': [('PINA', 0x00)], 'iterations': 5},
         {'inputs': [('PINA', 0x01)], 'iterations': 5}, 
         {'inputs': [('PINA', 0x00)], 'iterations': 5}],
-	'expected': [('PORTB', 0x01)]}]
+	'expected': [('PORTB', 0x01)]},
+	{'description': 'Halfway', 'steps': [
+	{'inputs': [('PINA', 0x01)], 'iterations': 5},
+        {'inputs': [('PINA', 0x00)], 'iterations': 5}],
+	'expected': [('PORTB', 0x02)]}
+	]
 
 # Optionally you can add a set of "watch" variables these need to be global or static and may need
 # to be scoped at the function level (for static variables) if there are naming conflicts. The 

8ab9a21fc004b4e9b217e08ae38b0487a2fdeaf4
dthecsguy <50031416+dthecsguy@users.noreply.github.com>
2020-08-06 19:53:55 -0700

    part 1

--- source/main.c
+++ source/main.c
@@ -12,12 +12,60 @@
 #include "simAVRHeader.h"
 #endif
 
+#define button (PINA & 0x01)
+
+enum States {START, OFF_UNPRESSED, OFF_PRESSED, ON_UNPRESSED, ON_PRESSED} state;
+unsigned char leds = 0;
+
+void tick(){
+    switch(state){  //Transitions
+        case START:
+        case OFF_UNPRESSED:
+            state = (button == 0x01) ? OFF_PRESSED : OFF_UNPRESSED;
+            break;
+            
+        case OFF_PRESSED:
+            state = (button == 0x01) ? OFF_PRESSED : ON_UNPRESSED;
+            break;
+            
+        case ON_UNPRESSED:
+            state = (button == 0x01) ? ON_PRESSED : ON_UNPRESSED;
+            break;
+            
+        case ON_PRESSED:
+            state = (button == 0x01) ? ON_PRESSED : OFF_UNPRESSED;
+            break;
+    }
+    
+    switch(state){  //Actions
+            case OFF_UNPRESSED:
+            leds = 0x01;
+            break;
+            
+        case OFF_PRESSED:
+            leds = 0x02;
+            break;
+            
+        case ON_UNPRESSED:
+            leds = 0x02;
+            break;
+            
+        case ON_PRESSED:
+            leds = 0x01;
+            break;
+    }
+}
+
 int main(void) {
     /* Insert DDR and PORT initializations */
+    DDRA = 0x00;    PORTA = 0xFF;
+    DDRB = 0xFF;    PORTB = 0x00;
+    
+    state = START;
 
     /* Insert your solution below */
     while (1) {
-
+        tick();
     }
     return 1;
 }
